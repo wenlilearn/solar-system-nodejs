d7c7503f17174bf4feffde4da29fc071a353350d
wenlilearn <wenlilearn@gmail.com>
2024-04-01 01:34:59 +0000

    ci: debug

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect('', {
-    user: 'superuser',
-    pass: 'SuperPassword',
+mongoose.connect(process.env.MONGO_URI, {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
